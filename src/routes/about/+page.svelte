78b3c9b555d0bbf93ca67711c75afe59d4cce820
Arden <arden.haldorson@gmail.com>
2025-03-18 18:25:45 +0100

    minor typography changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -14,7 +14,7 @@
 	>
 		<div class="flex flex-col gap-2">
 			<h2 class="fira-mono-medium text-2xl text-center">Who is Arden?</h2>
-			<p>
+			<p class="text-[0.95rem] lg:text-base">
 				Arden is a Front End developer living in Malmö, Sweden. <br /> He loves to read, play video
 				games and take care of his many plants. When he is not doing any of those things, or coding,
 				he likes to take walks by the sea, make mediocre latte art and play D&D.
@@ -26,7 +26,7 @@
 		</div>
 		<div class="flex flex-col gap-2 w-full bg-teal-800 rounded-xl p-3 text-center lg:bg-none lg:rounded-none lg:p-0 lg:text-left">
 			<h3 class="fira-mono-medium text-[1.15rem]">You can find Arden here</h3>
-			<ul class="flex flex-col gap-2 w-full mb-1 ml-2 lg:ml-0">
+			<ul class="flex flex-col gap-2 w-full mb-1 ml-2 lg:ml-0 text-[0.95rem] lg:text-base">
 				<li class="flex items-center gap-2">
 					<a
 						href="https://www.linkedin.com/in/arden-haldorson/"
@@ -42,7 +42,7 @@
 					>
 				</li>
 			</ul>
-			<p>Or contact him directly at <a href="mailto:arden.haldorson@gmail.com" target="_blank" class="underline underline-offset-3">arden.haldorson[at]gmail.com</a></p>
+			<p class="text-[0.95rem] lg:text-base">Or contact him directly at <a href="mailto:arden.haldorson@gmail.com" target="_blank" class="underline underline-offset-3">arden.haldorson[at]gmail.com</a></p>
 		</div>
 	</div>
 </section>
