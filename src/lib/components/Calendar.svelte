67166e1a842a45cc1615d5845e9c59d9b0a7188a
Arden <arden.haldorson@gmail.com>
2025-03-18 15:29:57 +0100

    added an about page, changed from icon to link in calendar menu instead

--- src/lib/components/Calendar.svelte
+++ src/lib/components/Calendar.svelte
@@ -55,7 +55,7 @@
 	</div>
 	<div class="flex flex-col gap-2">
 		<a
-			class="rounded-lg p-2 text-center tracking-widest shadow-md 
+			class="rounded-lg p-2 text-center tracking-widest shadow-md
 			{currentParams.year === menu.activeYear && currentParams.month === undefined
 				? 'bg-teal-700 cursor-default'
 				: 'bg-teal-950 hover:bg-teal-600'}"
