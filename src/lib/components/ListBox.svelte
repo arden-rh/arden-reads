30af2d73bf21c59b5ddf59c0cf1023640801a165
Arden <arden.haldorson@gmail.com>
2025-03-18 18:26:09 +0100

    changeing listbox to be able to show list of books with both title and author

--- src/lib/components/ListBox.svelte
+++ src/lib/components/ListBox.svelte
@@ -1,29 +1,56 @@
 <script lang="ts">
+	import type { ArrayOfItemType } from "./MonthPageContent.svelte";
+
 	interface Props {
-		arrayOfItems: string[];
+		arrayOfItems: string[] | ArrayOfItemType[];
+		// arrayOfBooks?: { title: string; author: string }[];
+		// arrayOfTitles?: string[];
 		header: string;
 		shortTitles?: boolean;
-	};
+	}
 
-	let { arrayOfItems, header, shortTitles }: Props = $props();
-	
+	let { arrayOfItems, header }: Props = $props();
+
+	let shortTitles: boolean = $state(false);
+	let books: ArrayOfItemType[] = $state([]);
+	let authors: string[] = $state([]);
+
+	if (header !== 'Books') {
+		shortTitles = true;
+		authors = arrayOfItems as string[];
+	} else if (header === 'Books') {
+		shortTitles = false;
+		books = arrayOfItems as ArrayOfItemType[];
+	}
+
 </script>
 
-<div
-	class="max-w-[600px] lg:max-w-full flex flex-col grow lg:pt-4 lg:pb-0"
->
-	<h3 class="fira-mono-medium text-xl xl:text-2xl mb-4 w-full text-center lg:text-left">{header}</h3>
+<div class="max-w-[600px] lg:max-w-full flex flex-col grow lg:pt-4 lg:pb-0">
+	<h3 class="fira-mono-medium text-xl xl:text-2xl mb-4 w-full text-center lg:text-left">
+		{header}
+	</h3>
 	<ol
 		class="columns-1 lg:columns-2 gap-4 break-words
 		{shortTitles ? 'lg:columns-3 xl:columns-4' : 'lg:columns-2 xl:columns-3'}"
 	>
-		{#each arrayOfItems as itemTitle, i}
-			<li class="text-sm flex items-center gap-2 mb-3 {shortTitles ? 'lg:mr-2 xl:mr-4' : 'lg:mr-2'} ">
-				<span class="bg-teal-950 p-2 rounded-md shrink-0 min-w-[2rem] text-center"
-					>{i + 1}</span
+		{#if header === 'Authors'}
+			{#each authors as author, i}
+				<li
+					class="text-sm flex items-center gap-2 mb-3 {shortTitles
+						? 'lg:mr-2 xl:mr-4'
+						: 'lg:mr-2'} "
 				>
-				<span class="break-words">{itemTitle}</span>
-			</li>
-		{/each}
+					<span class="bg-teal-950 p-2 rounded-md shrink-0 min-w-[2rem] text-center">{i + 1}</span>
+					<span class="break-words">{author}</span>
+				</li>
+			{/each}
+		{:else if header === 'Books'}
+			{#each books as book, i}
+				<li class="text-sm/tight flex items-center gap-2 mb-3 lg:mr-2">
+					<span class="bg-teal-950 p-2 rounded-md shrink-0 min-w-[2rem] text-center">{i + 1}</span>
+					<span class="break-words">{book.title}<span class="fira-mono-medium px-1 text-teal-300">—</span>{book.author}</span>
+				</li>
+			{/each}
+		{/if}
 	</ol>
 </div>
