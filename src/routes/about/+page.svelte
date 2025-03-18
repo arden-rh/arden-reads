a8acc4e4088ab766c0ad9b3cfe4a5fcd198ddd3c
Arden <arden.haldorson@gmail.com>
2025-03-18 15:29:57 +0100

    added an about page, changed from icon to link in calendar menu instead

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+</script>
+
+<section
+	class="col-start-1 col-end-7 row-span-2 flex flex-col justify-center items-center h-fit"
+>
+	<h1
+		class="text-[4rem] md:text-[8rem] md:leading-none xl:text-[10rem] italic rozha-one-regular text-center lg:mb-2 w-full"
+	>
+		About
+	</h1>
+	<div
+		class="flex flex-col items-center gap-4 justify-center w-[95%] lg:bg-teal-800 rounded-xl p-4 lg:p-6 max-w-[600px]"
+	>
+		<div class="flex flex-col gap-2">
+			<h2 class="fira-mono-medium text-2xl text-center">Who is Arden?</h2>
+			<p>
+				Arden is a Front End developer living in Malmö, Sweden. <br /> He loves to read, play video
+				games and take care of his many plants. When he is not doing any of those things, or coding,
+				he likes to take walks by the sea, make mediocre latte art and play D&D.
+				<span class="block mt-3">
+					He is currently looking for a job, so if you've enjoyed this webpage, please don't
+					hesitate to contact him.
+				</span>
+			</p>
+		</div>
+		<div class="flex flex-col gap-2 w-full bg-teal-800 rounded-xl p-3 text-center lg:bg-none lg:rounded-none lg:p-0 lg:text-left">
+			<h3 class="fira-mono-medium text-[1.15rem]">You can find Arden here</h3>
+			<ul class="flex flex-col gap-2 w-full mb-1 ml-2 lg:ml-0">
+				<li class="flex items-center gap-2">
+					<a
+						href="https://www.linkedin.com/in/arden-haldorson/"
+						target="_blank"
+						class="btn-basic bg-teal-950 hover:bg-teal-600">LinkedIn</a
+					>
+				</li>
+				<li class="flex items-center gap-2">
+					<a
+						href="https://github.com/arden-rh"
+						target="_blank"
+						class="btn-basic bg-teal-950 hover:bg-teal-600">Github (arden-rh)</a
+					>
+				</li>
+			</ul>
+			<p>Or contact him directly at <a href="mailto:arden.haldorson@gmail.com" target="_blank" class="underline underline-offset-3">arden.haldorson[at]gmail.com</a></p>
+		</div>
+	</div>
+</section>
