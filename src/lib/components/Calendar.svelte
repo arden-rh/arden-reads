b91926062f48f90226391d1e40ca92e3e068d820
Arden <arden.haldorson@gmail.com>
2025-03-18 15:29:57 +0100

    added an about page, changed from icon to link in calendar menu instead

--- src/lib/components/Calendar.svelte
+++ src/lib/components/Calendar.svelte
@@ -14,25 +14,35 @@
 
 	if (!currentParams.month) {
 		currentParams.month = months[monthNumber - 1].name;
-	};
+	}
 
 	function handleYearClick(year: number) {
 		menu.activeYear = year;
-	};
+	}
 
 	function resetParams() {
 		currentParams.month = undefined;
 		currentParams.year = undefined;
 		menu.open = false;
 		enableScroll();
-	};
-
+	}
 </script>
 
 <div class="fira-mono-regular w-full">
-	<div class="flex pb-1 mb-2 fira-mono-medium justify-between text-lg">
-		<a href="/" onclick={() => resetParams()} class="rounded-lg p-1 px-2 bg-teal-900 hover:bg-teal-600">HOME</a>
-		<div class="flex gap-2 justify-end">
+	<div class="flex pb-1 mb-2 justify-between text-lg">
+		<div class="flex gap-0.5 justify-start items-center lg:text-[1rem]">
+			<a
+				href="/"
+				onclick={() => resetParams()}
+				class="rounded-lg p-1 px-1.5 bg-teal-900 hover:bg-teal-600">HOME</a
+			>
+			<a
+				href="/about"
+				onclick={() => resetParams()}
+				class="rounded-lg p-1 px-1.5 bg-teal-900 hover:bg-teal-600">ABOUT</a
+			>
+		</div>
+		<div class="flex gap-2 justify-end fira-mono-medium">
 			{#each years as year}
 				<button
 					class="{year === menu.activeYear
@@ -45,7 +55,7 @@
 	</div>
 	<div class="flex flex-col gap-2">
 		<a
-			class="rounded-lg p-2 text-center tracking-widest shadow-md 
+			class="rounded-lg p-2 text-center tracking-widest shadow-md
 			{currentParams.year === menu.activeYear && currentParams.month === undefined
 				? 'bg-teal-700 cursor-default'
 				: 'bg-teal-950 hover:bg-teal-600'}"
@@ -82,9 +92,7 @@
 						{month.abr}
 					</a>
 				{:else}
-					<div
-						class="btn-basic bg-teal-900 text-gray-300 shadow-none"
-					>
+					<div class="btn-basic bg-teal-900 text-gray-300 shadow-none">
 						{month.abr}
 					</div>
 				{/if}
