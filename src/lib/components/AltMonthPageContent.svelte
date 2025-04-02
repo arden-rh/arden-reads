b3d3426862d961552fe37a6a15d394dcf3629c00
Arden <arden.haldorson@gmail.com>
2025-04-02 13:02:22 +0200

    added alt layout for a month with no books read, created a component for it

---
+++ src/lib/components/AltMonthPageContent.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import ButtonLink from '$lib/components/ButtonLink.svelte';
+
+	interface Props {
+		currentYear: string;
+		title: string;
+		isThisTheFuture: boolean;
+	}
+
+	let { currentYear, title, isThisTheFuture }: Props = $props();
+</script>
+
+<section
+	class="col-start-1 col-end-7 row-span-2 flex flex-col justify-center items-center gap-6 h-fit mt-6"
+>
+	<h2 class="rozha-one-regular text-4xl lg:text-5xl text-teal-200 text-center mb-2">
+		{title}
+	</h2>
+	<div
+		class="flex flex-col w-full lg:max-w-fit gap-4 lg:gap-4 items-center justify-center lg:bg-teal-900 lg:rounded-xl px-3 lg:p-6"
+	>
+		<p class="text-center text-sm lg:text-base">
+			{#if isThisTheFuture}
+				Well, I haven't read any books during this month (yet) in any case.
+			{:else}
+				Looks like I haven't read any books during this month.
+			{/if}
+		</p>
+		<ButtonLink
+			title="Go back to home"
+			linkName="/"
+			theme="teritary"
+			className="p-3 w-full lg:w-1/2 fira-mono-medium"
+			typeOfLink="internal"
+		/>
+		<p class="text-center text-sm lg:text-base mt-2">Or check out this year's reading so far</p>
+		<ButtonLink
+			title="Reading done in {currentYear}"
+			linkName="/{currentYear}"
+			theme="teritary"
+			className="p-3 w-full lg:w-1/2 fira-mono-medium"
+			typeOfLink="internal"
+		/>
+	</div>
+</section>
--- src/routes/[year=yearMatch]/[month=monthMatch]/+page.svelte
+++ src/routes/[year=yearMatch]/[month=monthMatch]/+page.svelte
@@ -8,6 +8,7 @@
 	import MonthPageHeader from '$lib/components/MonthPageHeader.svelte';
 
 	import { currentParams } from '../../../states.svelte';
+	import AltMonthPageContent from '$lib/components/AltMonthPageContent.svelte';
 
 	let { data }: { data: PageData } = $props();
 
@@ -21,42 +22,19 @@
 	} else {
 		favouriteBookTitle = undefined;
 	}
-	
 </script>
 
 {#if data.futureDate}
-	<section
-		class="col-start-1 col-end-7 row-span-2 flex flex-col justify-center items-center gap-6 h-fit mt-6"
-	>
-		<h2 class="rozha-one-regular text-3xl lg:text-4xl xl:text-5xl text-teal-200 text-center mb-2">
-			Trying to predict the future?
-		</h2>
-		<div
-			class="flex flex-col w-full lg:max-w-fit gap-4 lg:gap-4 items-center justify-center lg:bg-teal-900 lg:rounded-xl px-3 lg:p-6"
-		>
-			<p class="text-center text-sm lg:text-base">
-				Well, I haven't read any books during this month (yet) in any case.
-			</p>
-			<ButtonLink
-				title="Go back to home"
-				linkName="/"
-				theme="teritary"
-				className="p-3 w-full lg:w-1/2 fira-mono-medium"
-				typeOfLink="internal"
-			/>
-
-			<p class="text-center text-sm lg:text-base mt-2">Or check out this year's reading so far</p>
-			<ButtonLink
-				title="Reading done in {data.currentYear.toString()}"
-				linkName="/{data.currentYear}"
-				theme="teritary"
-				className="p-3 w-full lg:w-1/2 fira-mono-medium"
-				typeOfLink="internal"
-			/>
-		</div>
-	</section>
+	<AltMonthPageContent
+		currentYear={data.currentYear.toString()}
+		title="Trying to predict the future?"
+		isThisTheFuture
+	/>
 {:else}
 	<MonthPageHeader month={currentParams.month} />
-
-	<MonthPageContent {authors} {monthBooks} {favouriteBookTitle} {amountOfUniqueAuthors} />
+	{#if monthBooks.length === 0}
+		<AltMonthPageContent currentYear={data.currentYear.toString()} title="Hmm..." isThisTheFuture={false} />
+	{:else}
+		<MonthPageContent {authors} {monthBooks} {favouriteBookTitle} {amountOfUniqueAuthors} />
+	{/if}
 {/if}
